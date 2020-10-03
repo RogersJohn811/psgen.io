c83a98803fb5f35c63bc73726ef599ace16b0bc7
RogersJohn811 <RogersJohn811@gmail.com>
2020-10-02 20:10:29 -0400

    Edited js

--- script.js
+++ script.js
@@ -17,7 +17,33 @@ function generate(){
   //add password to textbox/display area
   document.getElementById("display").value = password;
 
+  //add password to previously generated passwords section
+  document.getElementById("lastNums").innerHTML += password + "<br />";
 
+}
 
+//set default length display of 25
+document.getElementById("length").innerHTML = "Length: 25";
+
+//function to set length based on slider position
+document.getElementById("slider").oninput = function(){
+
+  if(document.getElementById("slider").value > 0){
+      document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
+  }
+  else{
+      document.getElementById("length").innerHTML = "Length: 1";
+  }
+
+}
+
+//function to copy password to clipboard
+function copyPassword(){
+
+  document.getElementById("display").select();
+
+  document.execCommand("Copy");
+
+  alert("Password copied to clipboard!");
 
 }
